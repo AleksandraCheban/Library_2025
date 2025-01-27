e24bf36094d54cc16f657923dfbfe58cab8e3832
227104 <227104@KB37-128-C15>
2025-01-27 14:17:38 +0300

    New

--- Library_2025/Model.Context.cs
+++ Library_2025/Model.Context.cs
@@ -19,13 +19,14 @@ namespace Library_2025
             : base("name=Entities")
         {
         }
-    
-        private static Entities _context;
+
+        private  static Entities _context;
         public static Entities GetContext()
         {
             if (_context == null) _context = new Entities();
             return _context;
         }
+    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
