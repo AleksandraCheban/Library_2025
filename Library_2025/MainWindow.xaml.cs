409cd14e2e9b6d1ccb076d2f1ef4d3104b189242
227104 <227104@KB37-316-C02>
2025-02-27 13:32:42 +0300

    доделать регестраци.

--- Library_2025/MainWindow.xaml.cs
+++ Library_2025/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -33,46 +35,105 @@ namespace Library_2025
         
         private void Button_Reg_Click(object sender, RoutedEventArgs e)
         {
-            string login = textBoxLogin.Text.Trim();
-            string pass = passBox.Password.Trim();
-            string pass_2 = passBox_2.Password.Trim();
-            string email = textBoxEmail.Text.Trim().ToLower();
+            //string login = textBoxLogin.Text.Trim();
+            //string pass = passBox.Password.Trim();
+            //string pass_2 = passBox_2.Password.Trim();
+            //string email = textBoxEmail.Text.Trim().ToLower();
 
-            if (login.Length < 5)
-            {
-                textBoxLogin.ToolTip = "Это поле введено не корректно!";
-                textBoxLogin.Background = Brushes.DarkRed;
-            } else if (pass.Length < 5)
-            {
-                passBox.ToolTip = "Это поле введено не корректно!";
-                passBox.Background = Brushes.DarkRed;
+            //if (login.Length < 5)
+            //{
+            //    textBoxLogin.ToolTip = "Это поле введено не корректно!";
+            //    textBoxLogin.Background = Brushes.DarkRed;
+            //} else if (pass.Length < 5)
+            //{
+            //    passBox.ToolTip = "Это поле введено не корректно!";
+            //    passBox.Background = Brushes.DarkRed;
 
+            //}
+            //else if (pass != pass_2)
+            //{
+            //    passBox_2.ToolTip = "Это поле введено не корректно!";
+            //    passBox_2.Background = Brushes.DarkRed;
+
+            //}
+            //else if (email.Length<5 || !email.Contains("@") || !email.Contains("."))
+            //{
+            //    textBoxEmail.ToolTip = "Это поле введено не корректно!";
+            //    textBoxEmail.Background = Brushes.DarkRed;
+
+            //}
+            //else
+            //{
+            //    textBoxLogin.ToolTip = "";
+            //    textBoxLogin.Background = Brushes.Transparent;
+            //    passBox.ToolTip = "";
+            //    passBox.Background = Brushes.Transparent;
+            //    passBox_2.ToolTip = "";
+            //    passBox_2.Background = Brushes.Transparent;
+            //    textBoxEmail.ToolTip = "";
+            //    textBoxEmail.Background = Brushes.Transparent;
+
+            //    MessageBox.Show("Все хорошо!");
+            
+
+            if(textBoxLogin.Text.Length > 0)
+            {
+                using (var db = new Entities())
+                {
+                    var user = db.Users.AsNoTracking().FirstOrDefault(u => u.Login == textBoxLogin.Text);
+                    if (user != null) { MessageBox.Show("Пользователь с такими данными уже существует!"); return; }
+                }
+
+                bool en = true;
+                bool number = false;
+                for (int i = 0; 1 < passBox.Password.Length; i++)
+                {
+                    if (passBox.Password[i] >= 'A' && passBox.Password[i] <= 'Я') en = false;
+                    if (passBox.Password[i] >= '0' && passBox.Password[i] <= '9') number = true;
+                }
+                var regex = new Regex(@"*((\+7))\d{10}$");
+
+                StringBuilder errors = new StringBuilder();
+
+                if (passBox.Password.Length < 6) errors.AppendLine("Пароль должен быть больше 6 сиmвоnов"); 
+                if (!en) errors.AppendLine("Пароль должен быть на английском языкe");
+                if (!number) errors.AppendLine("Пароль должен содержать хотя бы одну цифру"); if (!isValidMail(textBoxEmail.Text)) errors.AppendLine("Введите кoрректный email");
+
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                else
+                {
+                    Entities db = new Entities();
+                    Users userObject = new Users
+                    {
+                        Login = textBoxLogin.Text,
+                        Password = GetHash(passBox.Password),
+                        Role = 1,
+                        E_mail = textBoxEmail.Text,
+                    };
+                    db.Users.Add(userObject);
+                    db.SaveChanges();
+                    MessageBox.Show("Вы успешно зарегистрировались!", "Успешно!", MessageBoxButton.OK);
+                    //NavigationService.Navigate((new Uri("/Pages/AuthLog.xaml", UriKind.Relative)));
+                }
             }
-            else if (pass != pass_2)
-            {
-                passBox_2.ToolTip = "Это поле введено не корректно!";
-                passBox_2.Background = Brushes.DarkRed;
+            else MessageBox.Show("Укажите лоигн!");
+        }
 
-            }
-            else if (email.Length<5 || !email.Contains("@") || !email.Contains("."))
+        public static string GetHash(string password)
+        {
+            using (var hash = SHA1.Create())
             {
-                textBoxEmail.ToolTip = "Это поле введено не корректно!";
-                textBoxEmail.Background = Brushes.DarkRed;
-
+                return string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(password)).Select(x => x.ToString("X2")));
             }
-            else
-            {
-                textBoxLogin.ToolTip = "";
-                textBoxLogin.Background = Brushes.Transparent;
-                passBox.ToolTip = "";
-                passBox.Background = Brushes.Transparent;
-                passBox_2.ToolTip = "";
-                passBox_2.Background = Brushes.Transparent;
-                textBoxEmail.ToolTip = "";
-                textBoxEmail.Background = Brushes.Transparent;
+        }
 
-                MessageBox.Show("Все хорошо!");
-            }
+        private bool isValidMail(object text)
+        {
+            throw new NotImplementedException()
 
         }
     }
