af486a00d762ad1a60c0061a4f20788a5ede4e6f
227104 <227104@KB37-120-C04>
2025-04-28 15:32:50 +0300

    Данные в таблицах не отображаются

--- Library_2025/MyOrders.xaml.cs
+++ Library_2025/MyOrders.xaml.cs
@@ -23,6 +23,40 @@ namespace Library_2025
         public MyOrders()
         {
             InitializeComponent();
+            DataGridOrders.ItemsSource = Library_2025Entities.GetContext().Orders.ToList();
+        }
+        private void ButtonEdit_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Логика для редактирования продукта
+            // Например, можно открыть страницу редактирования с выбранным продуктом
+            var selectedProduct = DataGridOrders.SelectedItem as Books;
+            if (selectedProduct != null)
+            {
+                //NavigationService.Navigate(new EditProductPage(selectedProduct)); 
+            }
+        }
+
+        private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new AddProductPage());
+        }
+
+        private void ButtonDel_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Логика для удаления продукта
+            var selectedProduct = DataGridOrders.SelectedItem as Books;
+            if (selectedProduct != null)
+            {
+                var context = Library_2025Entities.GetContext();
+                context.Books.Remove(selectedProduct);
+                context.SaveChanges();
+                DataGridOrders.ItemsSource = context.Books.ToList();
+            }
+        }
+
+        private void ButtonReturnToMain_OnClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
         }
     }
 }
