8092dfd07327d5e7139d0068391b1e2e34831632
AleksandraCheban <blueberry04p@gmail.com>
2025-04-29 13:50:46 +0300

    добавить скроллинг

--- Library_2025/PageForAdmin.xaml.cs
+++ Library_2025/PageForAdmin.xaml.cs
@@ -28,10 +28,15 @@ namespace Library_2025
 
         private void Books_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new BooksBageAdmin());
+            NavigationService.Navigate(new BooksPage());
         }
 
         private void Orders_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrdersPage());
+        }
+
+        private void Users_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new UsersPage());
         }
